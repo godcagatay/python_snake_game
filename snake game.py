38cfcc62fc05f18ccc96e3424788b5461b218229
godcagatay <138294925+godcagatay@users.noreply.github.com>
2023-08-15 17:51:14 +0300

    Add files via upload

---
+++ snake game.py
@@ -0,0 +1,125 @@
+import turtle
+import time
+import random
+
+# Oyun penceresi oluşturma
+wn = turtle.Screen()
+wn.title("Yılan Oyunu")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# Yılan başı
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Yemek
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Yılanın hareket fonksiyonları
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+# Yılanın hareketini güncelleme
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Klavye bağlantıları
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_left, "Left")
+wn.onkeypress(go_right, "Right")
+
+# Ana oyun döngüsü
+while True:
+    wn.update()
+    
+    # Sınırları kontrol etme
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+        
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        segments.clear()
+
+    # Yemek yediğinde segment eklemek
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+        
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("gray")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Segmentlerin hareketi
+    for i in range(len(segments) - 1, 0, -1):
+        x = segments[i - 1].xcor()
+        y = segments[i - 1].ycor()
+        segments[i].goto(x, y)
+    
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+    
+    move()
+    
+    # Yılanın kendisine çarpma kontrolü
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+            
+            for segment in segments:
+                segment.goto(1000, 1000)
+            
+            segments.clear()
+    
+    time.sleep(0.1)
+
+wn.mainloop()
